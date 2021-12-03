4f85ad2cb3c5806f51c35fc154b270973120dca9
Sinan Kara <sinankara@gmail.com>
2021-12-03 10:01:16 +0300

    AsyncImage tamamlandi

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let url = URL(string: "https://images.unsplash.com/flagged/1/apple-gear-looking-pretty.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80")
+    let url2 = URL(string: "https://picsum.photos/300/340")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            
+            // normal kullanim hali
+            AsyncImage(url: url2).cornerRadius(10)
+            Divider()
+            
+            
+            /* resize yapabilmek icin, we placeholder kullanabilmek icin
+            // we resizing as we need, thus we used
+            AsyncImage(url: <#T##URL?#>, content: <#T##(Image) -> View#>, placeholder: <#T##() -> View#>)
+            */
+            AsyncImage(url: url2) { returnedImage in
+                returnedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200, alignment: .center)
+            } placeholder: {
+                ProgressView()
+            }
+            
+            
+            Divider()
+            // AsyncImagePhase
+            AsyncImage(url: url2) { asyncImagePhase in
+                switch asyncImagePhase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150, alignment: .center)
+                case .failure:
+                    Image(systemName: "questionmark").font(.headline)
+                @unknown default:
+                    Image(systemName: "questionmark").font(.headline)
+                }
+            }
+
+        }
+        
     }
 }
 
